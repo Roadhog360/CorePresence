38345e84aa8945197fd540f4350a76fd88472e01
Roadhog360 <Roadhog360@outlook.com>
2024-01-22 00:08:56 -0600

    Change arena format to GTD instead of GameMap

--- src/gamedata/Arena.java
+++ src/gamedata/Arena.java
@@ -5,27 +5,27 @@ public enum Arena {
 
 	UNKNOWN_UNREGISTERED("", "unknown_stage", "Unknown"),
 
-	AHTEN_CITY("GameMapAhtenCity", "ahten_city", "Ahten City"),
-	AIMI_APP("GameMapDigitalWorld", "aimi_s_app", "Ai.Mi's App"),
-	NIGHT_MARKET("GameMapNightMarket", "night_market", "Night Market"),
-	ATLAS_LAB("GameMapAtlasLab", "atlas_lab", "Atlas's Lab"),
-	DEMON_DAIS("GameMapMusicStage", "demon_dais", "Demon Dais"),
-	ONI_VILLAGE("GameMapOniVillage", "oni_village", "Oni Village"),
-	INKY_SPLASH_ZONE("GameMapSummerSplash", "inky_s_splash_zone", "Inky's Splash Zone"),
-	GATES_OF_OBSCURA("GameMapObscura", "gates_of_obscura", "Gates of Obscura"),
-	CLARION_TEST_CHAMBER("GameMapClarionCorpDefault", "clarion_test_chamber_normal", "Clarion Test Chamber"),
-	TAIKO_TEMPLE("GameMapDrums", "taiko_temple", "Taiko Temple"),
+	AHTEN_CITY("GTD_AhtenCity", "ahten_city", "Ahten City"),
+	AIMI_APP("GTD_DigitalWorld", "aimi_s_app", "Ai.Mi's App"),
+	NIGHT_MARKET("GTD_NightMarket", "night_market", "Night Market"),
+	ATLAS_LAB("GTD_Lab", "atlas_lab", "Atlas's Lab"),
+	DEMON_DAIS("GTD_MusicStage", "demon_dais", "Demon Dais"),
+	ONI_VILLAGE("GTD_OniVillage", "oni_village", "Oni Village"),
+	INKY_SPLASH_ZONE("GTD_SummerSplash", "inky_s_splash_zone", "Inky's Splash Zone"),
+	GATES_OF_OBSCURA("GTD_Obscura", "gates_of_obscura", "Gates of Obscura"),
+	CLARION_TEST_CHAMBER("GTD_ClarionCorpDefault", "clarion_test_chamber_normal", "Clarion Test Chamber"),
+	TAIKO_TEMPLE("GTD_Drums", "taiko_temple", "Taiko Temple"),
 
-	CLARION_TEST_CHAMBER_BASSDROP("GameMapClarionCorpProjectThunderstruck", "clarion_test_chamber_project_bassdrop", "Clarion Test Chamber | Project: Bassdrop"),
-	CLARION_TEST_CHAMBER_EXPANSE("GameMapClarionCorpProjectExpanse", "clarion_test_chamber_project_expanse", "Clarion Test Chamber | Project: Expanse"),
-	CLARION_TEST_CHAMBER_MAELSTROM("GameMapClarionCorpProjectMaelstrom", "clarion_test_chamber_project_maelstrom", "Clarion Test Chamber | Project: Maelstrom"),
-	CLARION_TEST_CHAMBER_OBSCURA("GameMapClarionCorpProjectObscura", "clarion_test_chamber_project_obscura", "Clarion Test Chamber | Project: Obscura"),
-	CLARION_TEST_CHAMBER_XENO("GameMapClarionCorpProjectXeno", "clarion_test_chamber_project_xeno", "Clarion Test Chamber | Project: Xeno"),
+	CLARION_TEST_CHAMBER_BASSDROP("GTD_ClarionCorpProjectThunderstruck", "clarion_test_chamber_project_bassdrop", "Clarion Test Chamber | Project: Bassdrop"),
+	CLARION_TEST_CHAMBER_EXPANSE("GTD_ClarionCorpProjectExpanse", "clarion_test_chamber_project_expanse", "Clarion Test Chamber | Project: Expanse"),
+	CLARION_TEST_CHAMBER_MAELSTROM("GTD_ClarionCorpProjectMaelstrom", "clarion_test_chamber_project_maelstrom", "Clarion Test Chamber | Project: Maelstrom"),
+	CLARION_TEST_CHAMBER_OBSCURA("GTD_ClarionCorpProjectObscura", "clarion_test_chamber_project_obscura", "Clarion Test Chamber | Project: Obscura"),
+	CLARION_TEST_CHAMBER_XENO("GTD_ClarionCorpProjectXeno", "clarion_test_chamber_project_xeno", "Clarion Test Chamber | Project: Xeno"),
 
-	ABOUT_FACE("GameMapAboutFaceXL", "about_face", "About Face"),
-	CORNER_POCKET("GameMapCornerGoalXL", "corner_pocket", "Corner Pocket"),
-	MAP_FLIPPED("GameMapFlippedXL", "map_flipped", "Map Flipped"),
-	BACK_TO_BACK("GameMapBackToBackXL", "back_to_back", "Back to Back"),
+	ABOUT_FACE("GTD_AboutFaceXL", "about_face", "About Face"),
+	CORNER_POCKET("GTD_CornerGoalXL", "corner_pocket", "Corner Pocket"),
+	MAP_FLIPPED("GTD_FlippedXL", "map_flipped", "Map Flipped"),
+	BACK_TO_BACK("GTD_BackToBackXL", "back_to_back", "Back to Back"),
 	;
 
 	private final String internalName;
@@ -55,7 +55,7 @@ public enum Arena {
 			return AHTEN_CITY;
 		}
 		for(Arena arena : values()) {
-			if(arena.getInternalName().replace("GameMap", "").equals(name)) {
+			if(arena.getInternalName().replace("GTD_", "").equals(name)) {
 				return arena;
 			}
 		}
